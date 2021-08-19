fb8f698fc7333a370d89fd2fa7f94cab5fc3f384
mjjstockman <mjjstockman@gmail.com>
2021-08-19 13:58:50 +0000

    Display addition question the the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("Submit clicked");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`Game type is ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
-    let num1 = Math.floor(Math.random() * 25);
-    let num2 = Math.floor(Math.random() * 25);
+function runGame(gameType) {
+    // Create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`
+    }
+    
 }
 
 function checkAnswer() {
@@ -39,8 +50,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
